bd5f0456ffe08f0043142d351b40b2c79c1f8fa7
Volodymyr <wolodymyrs@gmail.com>
2021-09-16 00:34:39 +0300

    functionality close modal and destroy setContemt edded

--- myJS_usingJS/modal-andJS/index.js
+++ myJS_usingJS/modal-andJS/index.js
@@ -1,1 +1,13 @@
-const modal = $.modal();
+// const fruits = [
+//   { id: 1, title: apples, price: 20 },
+//   { id: 2, title: oranges, price: 30 },
+//   { id: 3, title: mangoes, price: 40 },
+// ];
+
+const modal = $.modal({
+  title: "Volodymyr's Modal",
+  closable: true,
+  content: `<h4>This Modal is shown and is working!</h4>
+    <p>Lorem ipsum dolor sit amet.</p>`,
+  width: '400px',
+});
--- myJS_usingJS/modal-andJS/plugins/modal.js
+++ myJS_usingJS/modal-andJS/plugins/modal.js
@@ -2,37 +2,13 @@ $.modal = function (options) {
   const $modal = _createModal(options);
   const ANIMATION_SPEED = 200;
   let closing = false;
+  let destroyed = false;
 
-  function _createModal(options) {
-    const modal = document.createElement('div');
-    modal.classList.add('vmodal');
-    modal.insertAdjacentHTML(
-      'afterbegin',
-      `
-      <div class="modal-overlay">
-        <div class="modal-window">
-          <div class="modal-header">
-            <span class="modal-title">Some Title</span>
-            <span class="modal-close">&times;</span>
-          </div>
-          <div class="modal-body">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-          </div>
-          <div class="modal-footer">
-            <button>OK</button>
-            <button>Cansel</button>
-          </div>
-        </div>
-      </div>
-    `
-    );
-    document.body.appendChild(modal);
-    return modal;
-  }
-
-  return {
+  const modal = {
     open() {
+      if (destroyed) {
+        return console.log('Modal is destroyed');
+      }
       !closing && $modal.classList.add('open');
     },
     close() {
@@ -44,6 +20,64 @@ $.modal = function (options) {
         closing = false;
       }, ANIMATION_SPEED);
     },
-    destroy() {},
+    // destroy() {}, -> you can put this method here or use Object.assign
   };
+
+  const listener = (event) => {
+    if (event.target.dataset.close) {
+      modal.close();
+    }
+  };
+
+  $modal.addEventListener('click', listener);
+
+  function _createModal(options) {
+    const DEFAULT_WIDTH = '600px';
+    const modal = document.createElement('div');
+    modal.classList.add('vmodal');
+    modal.insertAdjacentHTML(
+      'afterbegin',
+      `
+        <div class="modal-overlay" data-close="true">
+          <div class="modal-window" style="width: ${
+            options.width || DEFAULT_WIDTH
+          }">
+            <div class="modal-header">
+              <span class="modal-title">${
+                options.title || 'Here is TITLES'
+              }</span>
+              ${
+                options.closable
+                  ? `<span class="modal-close" data-close="true">&times;</span>`
+                  : ''
+              }
+            </div>
+            <div class="modal-body" data-content>
+              ${options.content || ''}
+            </div>
+            <div class="modal-footer">
+              <button>OK</button>
+              <button>Cansel</button>
+            </div>
+          </div>
+        </div>
+      `
+    );
+    document.body.appendChild(modal);
+    return modal;
+  }
+
+  return Object.assign(modal, {
+    // to remove whole modal, clean and save memory
+    destroy() {
+      destroyed = true;
+      // to remove elements from DOM-tree
+      $modal.parentNode.removeChild($modal);
+      // to remove eventListener
+      $modal.removeEventListener('click', listener);
+    },
+    setContent(html) {
+      $modal.querySelector('[data-content]').innerHTML = html;
+    },
+  });
 };
